fd05437e3e220a0cb6ad3b3c97984a9fd66114a1
HarshAProgrammer <App.Lavishly@Gmail.com>
2021-09-21 19:28:13 +0530

    Initial Commit

---
+++ app/src/main/java/org/schabi/newpipe/local/dialog/PlaylistCreationDialog.java
@@ -0,0 +1,71 @@
+package org.schabi.newpipe.local.dialog;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.text.InputType;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
+import org.schabi.newpipe.NewPipeDatabase;
+import org.schabi.newpipe.R;
+import org.schabi.newpipe.database.stream.model.StreamEntity;
+import org.schabi.newpipe.databinding.DialogEditTextBinding;
+import org.schabi.newpipe.local.playlist.LocalPlaylistManager;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+
+public final class PlaylistCreationDialog extends PlaylistDialog {
+    public static PlaylistCreationDialog newInstance(final List<StreamEntity> streams) {
+        final PlaylistCreationDialog dialog = new PlaylistCreationDialog();
+        dialog.setInfo(streams);
+        return dialog;
+    }
+
+    public static PlaylistCreationDialog newInstance(final PlaylistAppendDialog appendDialog) {
+        final PlaylistCreationDialog dialog = new PlaylistCreationDialog();
+        dialog.setInfo(appendDialog.getStreams());
+        return dialog;
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+    // Dialog
+    //////////////////////////////////////////////////////////////////////////*/
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable final Bundle savedInstanceState) {
+        if (getStreams() == null) {
+            return super.onCreateDialog(savedInstanceState);
+        }
+
+        final DialogEditTextBinding dialogBinding
+                = DialogEditTextBinding.inflate(getLayoutInflater());
+        dialogBinding.dialogEditText.setHint(R.string.name);
+        dialogBinding.dialogEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(requireContext())
+                .setTitle(R.string.create_playlist)
+                .setView(dialogBinding.getRoot())
+                .setCancelable(true)
+                .setNegativeButton(R.string.cancel, null)
+                .setPositiveButton(R.string.create, (dialogInterface, i) -> {
+                    final String name = dialogBinding.dialogEditText.getText().toString();
+                    final LocalPlaylistManager playlistManager =
+                            new LocalPlaylistManager(NewPipeDatabase.getInstance(requireContext()));
+                    final Toast successToast = Toast.makeText(getActivity(),
+                            R.string.playlist_creation_success,
+                            Toast.LENGTH_SHORT);
+
+                    playlistManager.createPlaylist(name, getStreams())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(longs -> successToast.show());
+                });
+
+        return dialogBuilder.create();
+    }
+}
